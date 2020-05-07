1c99ff0b022f706645e2a805435631bf4ceb538d
kyrillos <kyrillos417@gmail.com>
2020-05-07 12:06:36 -0700

    Create TermsPage/Add Some animi on home screen / edit scripts for booking page

--- js/app.js
+++ js/app.js
@@ -16,32 +16,21 @@ $('.owl-carousel').owlCarousel({
     }
 });
 
-//Questions Script 
+/****************SCript Questions in Booking Page *****************/
 $('.Details .inner button').click(function(){
-    console.log($('.step'));
     if($('.Details .inner').last().css('display') == 'none'){
         $(this).parent().fadeOut(function(){
             $(this).next().fadeIn().css('display' , 'flex');
-            $('.'+$(this).data('class') +'').removeClass('activeNum').css('backgroundColor' , 'Green');
-            $('.'+$(this).next().data('class') +'').addClass('activeNum')
+            $('.'+$(this).data('class') +'').removeClass('--current').addClass('--completed');
+            $('.'+$(this).next().data('class') +'').removeClass('--pending').addClass('--current')
         });
     }else{
         $('.inner button').text('Done');
     }
-    // if($('.step:eq(0)').hasClass('--current')){
-    //     $('.step:eq(0)').removeClass('--current').addClass('--completed').next().removeClass('--pending').addClass('--current');
-    // }
-    console.log($('.step').length)
-    for(let i = 0 ; i < $('.step').length ; i++){
-        if($('.step:eq('+i+')').hasClass('--current')){
-            $('.step:eq('+i+')').removeClass('--current').addClass('--completed').next().removeClass('--pending');
-            // if($('.step:eq('+i+')').hasClass('--current'))
-            // $('.step:eq('+(i + 1)+')').removeClass('--pending').addClass('--current')
-        }
-    }
-
 });
-//Modein Select Country in Booking page
+
+
+//Mode in Select Country in Booking page
 (function(){
     for(let i = 0 ; i < $('.modal ul li').length; i++){
         $('.modal ul li:eq('+i+')').click(function(){
@@ -50,16 +39,6 @@ $('.Details .inner button').click(function(){
             $('body').removeClass('modal-open')
         })
     }
-
-
-    //SCript For Questions in Dry Page
-    // for(let i =0 ; i < $('.Dry .Questions .collapse ').length ; i++ ){
-    //     if($('.Dry .Questions .collapse:eq('+i+')').hasClass('show')){
-    //         console.log($('.Dry .Questions .collapse ').length)
-    //         $('.Dry .Questions .collapse:eq('+i+')').prev().css('backgroundColor' , 'rgb(46, 179, 183)').firs
-    //     }
-        
-    // }
 })();
 // Data Picker in Booking page
 //Date and time picker 
